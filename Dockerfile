2614eff43111c506f8c2b85dda752bc054cf2800
Camilo Diaz <diazc.pr7966@gmail.com>
2024-02-01 02:01:04 -0500

    se cambio la base de datos h2 para que no guarde datos en disco, debido a que render no lo permite

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ RUN mvn clean package -DskipTests
 
 FROM openjdk:17.0.1-jdk-slim
 COPY --from=build /target/cdgym-0.0.1-SNAPSHOT.jar cdgym-app.jar
-EXPOSE 8080
+EXPOSE 8081
 ENTRYPOINT ["java","-jar","cdgym-app.jar"]
--- src/main/java/cdgym/CdgymApplication.java
+++ src/main/java/cdgym/CdgymApplication.java
@@ -2,12 +2,30 @@ package cdgym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import cdgym.entities.Cliente;
+import cdgym.entities.Empleado;
+import cdgym.entities.Usuario;
+import cdgym.service.ClienteService;
+import cdgym.service.EmpleadosService;
+import cdgym.service.UsuarioService;
 
 @SpringBootApplication
 public class CdgymApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CdgymApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(CdgymApplication.class, args);
+		UsuarioService usuarioService = run.getBean(UsuarioService.class);
+			EmpleadosService service = run.getBean(EmpleadosService.class);
+			ClienteService clienteService = run.getBean(ClienteService.class);
+			PasswordEncoder encoder = run.getBean(PasswordEncoder.class);
+			Empleado empleado = new Empleado("Administrador", "Ramiro", "Quevedo", 1123578920);
+			Usuario usuario = new Usuario("ramiro", encoder.encode("password"), empleado);
+			usuario.setRole(usuarioService.gestionarRole(empleado.getCargo()));
+			empleado.setUsuario(usuario);
+			service.save(empleado);
+			clienteService.saveCliente(new Cliente("camilo", "diaz", 1111111111, 3183183183l, null, null));
+		
 	}
 }
