582cb00495723830bf632d0b6bfd5d03ac406304
Camilo Diaz <diazc.pr7966@gmail.com>
2024-02-01 01:30:58 -0500

    se agrego el dockerfile y se borro codigo del main

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM maven:3.8.5-openjdk-17 AS build
+COPY . .
+RUN mvn clean package -DskipTests
+
+FROM openjdk:17.0.1-jdk-slim
+COPY --from=build /target/cdgym-0.0.1-SNAPSHOT.jar cdgym-app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","cdgym-app.jar"]
--- src/main/java/cdgym/CdgymApplication.java
+++ src/main/java/cdgym/CdgymApplication.java
@@ -2,35 +2,12 @@ package cdgym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import cdgym.entities.Cliente;
-import cdgym.entities.Empleado;
-import cdgym.entities.Usuario;
-import cdgym.service.ClienteService;
-import cdgym.service.EmpleadosService;
-import cdgym.service.UsuarioService;
+
 
 @SpringBootApplication
 public class CdgymApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext run = SpringApplication.run(CdgymApplication.class, args);
-		System.out.println("empezando");
-		EmpleadosService service = run.getBean(EmpleadosService.class);
-		UsuarioService usuarioService = run.getBean(UsuarioService.class);
-		ClienteService clienteService = run.getBean(ClienteService.class);
-		PasswordEncoder encoder = run.getBean(PasswordEncoder.class);
-		Empleado empleado = new Empleado("Administrador", "Ramiro", "Quevedo", 1123578920);
-		Usuario usuario = new Usuario("ramiro", encoder.encode("password"), empleado);
-		usuario.setRole(usuarioService.gestionarRole(empleado.getCargo()));
-		empleado.setUsuario(usuario);
-		service.save(empleado);
-		System.out.println(empleado);
-		usuario = usuarioService.getUsuario(1l);
-		if(usuario != null) System.out.println(usuario);
-		else System.out.println("usuario no");
-		clienteService.saveCliente(new Cliente("camilo","diaz",1111111111,3183183183l,null,null));
-		
+		SpringApplication.run(CdgymApplication.class, args);
 	}
 }
